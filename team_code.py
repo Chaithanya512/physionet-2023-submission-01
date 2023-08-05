3023d991c1d29aa39b5a0b7c910a8f44593b3c9f
chaithanya <chay5522kalyan@gmail.com>
2023-08-06 02:38:00 +0530

    xgboost using metadata

---
+++ team_code.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+
+# Edit this script to add your team's code. Some functions are *required*, but you can edit most parts of the required functions,
+# change or remove non-required functions, and add your own functions.
+
+################################################################################
+#
+# Optional libraries, functions, and variables. You can change or remove them.
+#
+################################################################################
+
+from helper_code import *
+import numpy as np, os, sys
+from sklearn.impute import SimpleImputer
+import xgboost as xgb
+from sklearn.model_selection import GridSearchCV
+import joblib
+
+################################################################################
+#
+# Required functions. Edit these functions to add your code, but do not change the arguments of the functions.
+#
+################################################################################
+
+# Train your model.
+def train_challenge_model(data_folder, model_folder, verbose):
+    # Find data files.
+    if verbose >= 1:
+        print('Finding the Challenge data...')
+
+    patient_ids = find_data_folders(data_folder)
+    num_patients = len(patient_ids)
+
+    if num_patients==0:
+        raise FileNotFoundError('No data was provided.')
+
+    # Create a folder for the model if it does not already exist.
+    os.makedirs(model_folder, exist_ok=True)
+
+    # Extract the features and labels.
+    if verbose >= 1:
+        print('Extracting features and labels from the Challenge data...')
+
+    features = list()
+    outcomes = list()
+    cpcs = list()
+
+    for i in range(num_patients):
+        if verbose >= 2:
+            print('    {}/{}...'.format(i+1, num_patients))
+
+        current_features = get_features(data_folder, patient_ids[i])
+        features.append(current_features)
+
+        # Extract labels.
+        patient_metadata = load_challenge_data(data_folder, patient_ids[i])
+        current_outcome = get_outcome(patient_metadata)
+        outcomes.append(current_outcome)
+        current_cpc = get_cpc(patient_metadata)
+        cpcs.append(current_cpc)
+
+    features = np.vstack(features)
+    outcomes = np.vstack(outcomes)
+    cpcs = np.vstack(cpcs)
+
+    # Train the models.
+    if verbose >= 1:
+        print('Training the Challenge model on the Challenge data...')
+
+    
+    # Define the hyperparameter grids for XGBoost classifier and regressor
+    param_grid_clf = {
+        'n_estimators':[250,275,300],
+        'max_depth': [3, 5],
+        'learning_rate': [0.1, 0.01]
+    }
+    
+    param_grid_reg = {
+         'n_estimators':[300,325,350],
+        'max_depth': [3, 5],
+        'learning_rate': [0.1, 0.01]
+    }
+    
+    
+    random_state   = 789  # Random state; set for reproducibility.
+
+    # Impute any missing features; use the mean value by default.
+    imputer = SimpleImputer().fit(features)
+    
+    features = imputer.transform(features)
+
+    
+    # Train the XGBoost classifier with hyperparameter tuning.
+    xgb_clf = xgb.XGBClassifier(random_state=random_state)
+    grid_search_clf = GridSearchCV(xgb_clf, param_grid=param_grid_clf, verbose=3,cv=5)
+    grid_search_clf.fit(features, outcomes.ravel())
+    best_xgb_clf = grid_search_clf.best_estimator_
+    
+    # Train the XGBoost regressor with hyperparameter tuning.
+    xgb_reg = xgb.XGBRegressor(random_state=random_state)
+    grid_search_reg = GridSearchCV(xgb_reg, param_grid=param_grid_reg,verbose=3, cv=5)
+    grid_search_reg.fit(features, cpcs.ravel())
+    best_xgb_reg = grid_search_reg.best_estimator_
+    
+    # Fit the best parameters to the models.
+    outcome_model = best_xgb_clf.fit(features, outcomes.ravel())
+    cpc_model = best_xgb_reg.fit(features, cpcs.ravel())
+
+
+    # Save the models.
+    save_challenge_model(model_folder, imputer, outcome_model, cpc_model)
+
+    if verbose >= 1:
+        print('Done.')
+
+# Load your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
+# arguments of this function.
+def load_challenge_models(model_folder, verbose):
+    filename = os.path.join(model_folder, 'models.sav')
+    return joblib.load(filename)
+
+# Run your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
+# arguments of this function.
+def run_challenge_models(models, data_folder, patient_id, verbose):
+    imputer = models['imputer']
+    outcome_model = models['outcome_model']
+    cpc_model = models['cpc_model']
+
+    # Extract features.
+    features = get_features(data_folder, patient_id)
+    features = features.reshape(1, -1)
+
+    # Impute missing data.
+    features = imputer.transform(features)
+
+    # Apply models to features.
+    outcome = outcome_model.predict(features)[0]
+    outcome_probability = outcome_model.predict_proba(features)[0, 1]
+    cpc = cpc_model.predict(features)[0]
+
+    # Ensure that the CPC score is between (or equal to) 1 and 5.
+    cpc = np.clip(cpc, 1, 5)
+
+    return outcome, outcome_probability, cpc
+
+################################################################################
+#
+# Optional functions. You can change or remove these functions and/or add new functions.
+#
+################################################################################
+
+# Save your trained model.
+def save_challenge_model(model_folder, imputer, outcome_model, cpc_model):
+    d = {'imputer': imputer, 'outcome_model': outcome_model, 'cpc_model': cpc_model}
+    filename = os.path.join(model_folder, 'models.sav')
+    joblib.dump(d, filename, protocol=0)
+
+
+# Extract features.
+def get_features(data_folder, patient_id):
+    # Load patient data.
+    patient_metadata = load_challenge_data(data_folder, patient_id)
+
+    # Extract patient features.
+    patient_features = get_patient_features(patient_metadata)
+
+    # Extract features.
+    return np.hstack((patient_features))
+
+# Extract patient features from the data.
+def get_patient_features(data):
+    age = get_age(data)
+    sex = get_sex(data)
+    rosc = get_rosc(data)
+    ohca = get_ohca(data)
+    shockable_rhythm = get_shockable_rhythm(data)
+    ttm = get_ttm(data)
+
+    sex_features = np.zeros(2, dtype=int)
+    if sex == 'Female':
+        female = 1
+        male   = 0
+        other  = 0
+    elif sex == 'Male':
+        female = 0
+        male   = 1
+        other  = 0
+    else:
+        female = 0
+        male   = 0
+        other  = 1
+
+    features = np.array((age, female, male, other, rosc, ohca, shockable_rhythm, ttm))
+
+    return features
+
